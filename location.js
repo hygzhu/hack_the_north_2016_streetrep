bf1fcd4507510c2419acdfaf694e12e596428921
Matthew <matthew@weme.sh>
2016-09-17 17:19:38 -0400

    get post within location fn

---
+++ location.js
@@ -0,0 +1,17 @@
+//How do i even write json, lol..
+
+var express = require('express')
+var geolib = require('geolib')
+
+function getPostsWithinRadius(userLat, userLng, posts) {
+  const radius = 10;
+  var q = [];
+  for p in posts:
+    var dist = geolib.getDistance(
+      {latitude: userLat, longitude: userLng},
+      {latitude: posts.latitude, longitude: posts.longitude}
+    );
+    if (dist <= radius) {
+      q.append(p)
+    }
+}
